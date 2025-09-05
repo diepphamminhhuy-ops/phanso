4d279807ccb72202b7cd0122699b74d4488201f0
diepphamminhhuy-ops <diepphamminhhuy@gmail.com>
2025-09-05 15:31:34 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    while (true){
+    cout << "Nhap tu so: ";
+    cin >> a;
+    cout << "Nhap mau so: ";
+    cin >> b;
+    
+    if ( a != (int)a || b != (int)b ){
+        cout << "Error" << endl;
+        continue;
+    }
+    cout << "Phan so duoc nhap la:" << a << "/" << b << endl;
+    
+
+    //xet dk
+    if (b == 0 ){
+
+        cout << "Error" << endl;
+        continue;
+    }
+     if (a == 0){
+
+        cout << "phan so duoc nhap la: " << a << "/" << b << endl;
+        cout << "phan so sau khi rut la: " << a << endl;
+        return 1;
+    }
+
+    if ( a < 0 || b < 0){
+
+        a = -a;
+        b = -b;
+        break;
+    }
+    
+    if (b != 0){
+        break;
+    }
+}
+    
+
+    //Rut gon phan so
+    int A, B;  // Tu va mau so sau khi rut gon
+
+    for (int i = 1; i <= a && i <= b; i++){
+        
+            
+            // c la uoc chung lon nhat
+            int c = i;
+
+            //Rut gon phan so
+            if (a % c == 0 && b % c == 0){
+                A = a / c;
+                B = b / c;
+            } else {
+                A = a;
+                B = b;
+            }
+        }
+    
+    
+   
+    
+    // In ket qua
+    
+    
+    cout << "Phan so sau khi rut gon la: " << A << "/" << B << endl;
+
+    return 0;
+}
